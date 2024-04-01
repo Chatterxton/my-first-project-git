9acb778b339cf9b65905d395c65c8222747f549d
Egor Kuznetsov <egorkuznecov470@gmail.com>
2024-04-01 15:37:53 +0300

    init commit ver 0.1

--- individual/Header.h
+++ individual/Header.h
@@ -1,6 +1,9 @@
 ﻿#ifndef HEADER_H
 #define HEADER_H
 
-void checkuser();
+void category(int &fl);
+void pc_turning_problems();
+void pc_program_problems();
+void pc_wifi_internet_wifi_bluetooth_problems();
 
 #endif
